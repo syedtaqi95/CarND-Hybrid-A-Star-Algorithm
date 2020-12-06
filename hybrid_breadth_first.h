0373cfb00b152cd24c12435006415b77994c9e39
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2020-12-06 14:04:28 +0000

    Implementation complete

--- hybrid_breadth_first.h
+++ hybrid_breadth_first.h
@@ -16,6 +16,7 @@ class HBF {
   // HBF structs
   struct maze_s {
     int g;  // iteration
+    int f;
     double x;
     double y;
     double theta;
@@ -31,8 +32,12 @@ class HBF {
   int theta_to_stack_number(double theta);
 
   int idx(double float_num);
+    
+  double heuristic(double x, double y, vector<int> &goal);
 
-  vector<maze_s> expand(maze_s &state);
+  static bool compare_maze_s(const HBF::maze_s &lhs, const HBF::maze_s &rhs);
+
+  vector<maze_s> expand(maze_s &state, vector<int> &goal);
 
   vector<maze_s> reconstruct_path(vector<vector<vector<maze_s>>> &came_from, 
                                   vector<double> &start, HBF::maze_s &final);
